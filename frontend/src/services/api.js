df089c35fbbb91c749022532e7475f6144ddd182
tahirabatool218-uoe <tahirabatool218@gmail.com>
2026-09-12 16:18:05 +0500

    Update production API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+baseURL: "https://apartment-tenant-management-system-production.up.railway.app/api",
   headers: {
     "Content-Type": "application/json",
   },
